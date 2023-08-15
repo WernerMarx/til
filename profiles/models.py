4264be83328890c0b1fdce9f3d598b9522343163
“WernerMarx” <werner.marx@outlook.com>
2023-08-14 20:32:53 -0600

    added profile admin

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Profile(models.Model):
-    user = 
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        # related_name="profile"
+    )
+
+
+    def __str__(self) -> str:
+        return self.user.username
