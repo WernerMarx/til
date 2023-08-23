774c000419bc3a8e7c77e8c47534c52a03a2116a
Werner Marx <werner.marx@outlook.com>
2023-08-23 13:23:30 -0600

    added post count and followers app

--- feed/views.py
+++ feed/views.py
@@ -4,34 +4,51 @@ from django.http.response import HttpResponse
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
+from django.shortcuts import render
 
 from .models import Post
 
+
 class HomePage(ListView):
     http_method_names = ["get"]
-    template_name = 'feed/home.html'
+    template_name = "feed/home.html"
     model = Post
     context_object_name = "posts"
-    queryset = Post.objects.all().order_by('-id')[0:30]
+    queryset = Post.objects.all().order_by("-id")[0:30]
+
 
 class PostDetailView(DetailView):
     http_method_names = ["get"]
-    template_name = 'feed/detail.html'
+    template_name = "feed/detail.html"
     model = Post
     context_object_name = "post"
+
 
 class CreateNewPost(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "feed/create.html"
-    fields = ['text']
+    fields = ["text"]
     success_url = "/"
 
     def dispatch(self, request, *args, **kwargs):
         self.request = request
         return super().dispatch(request, *args, **kwargs)
 
-    def form_valid(self,form):
+    def form_valid(self, form):
         obj = form.save(commit=False)
         obj.author = self.request.user
         obj.save()
         return super().form_valid(form)
+
+    def post(self, request, *args, **kwargs):
+        post = Post.objects.create(text=request.POST.get("text"), author=request.user)
+
+        return render(
+            request,
+            "includes/post.html",
+            {
+                "post": post,
+                "show_detail_link": True,
+            },
+            content_type="application/html",
+        )
--- til/urls.py
+++ til/urls.py
@@ -4,12 +4,14 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path, re_path
 from feed import urls as feed_urls
+from profiles import urls as profile_urls
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(feed_urls, namespace="feed")),
+    path('profile/', include(profile_urls, namespace="profiles")),
     re_path('', include('allauth.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
